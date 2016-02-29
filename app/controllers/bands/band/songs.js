ad1196b1b0f266fb231dad9beffa50707b1dd366
trebek1 <trebek08@gmail.com>
2016-02-28 19:15:19 -0800

    Added pressing return to submit the form for the adding of songs and to show adding of songs for new bands

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	songCreationStarted: false,
+	noSongs: Ember.computed('model.songs.length', function(){
+		return this.get('model.songs.length') === 0; 
+	}),
+	 canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  	}),
 	actions: {
+		enableSongCreation: function(){
+			this.set('songCreationStarted', true)
+		},
 		updateRating: function(params){
 			var song = params.item, 
 			rating = params.rating; 
